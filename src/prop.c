eb4f282797fc880f9dd8fd97fcc60a1147eb8bcf
Johan <johan@cgeek.net>
2011-05-12 12:09:51 +0200

    Added while loop so that prop thread will not exit

--- src/prop.c
+++ src/prop.c
@@ -22,43 +22,56 @@ prop_thread( void* data )
 
 	printf( "[Prop Thread] Thread started\n" );
 
-	/* locks the signal variable and waits */
-	pthread_mutex_lock( prop_sig_lock );
-	pthread_cond_wait( prop_sig, prop_sig_lock  );
-	pthread_mutex_unlock( prop_sig_lock );
-
-	printf( "[Prop Thread] Recevied signal \n" );
-
-	/* Check if propagation has already been performed */
-	if( cs->prop_gen == -1 )
+	while( 1 )
 	{
-		
-		
-		/* Start at the begining */
-		start_gen = 0;
-		start_pos = 0;
-		
-	}	
-	
-	/* Fetch the newest generaton that have been inserted */
-	end_gen = cs->max_gen;
-	
-	for( it = start_gen; it <= end_gen; it++ )
-	{
-		/* Fetch the generation */
-		g = cs->gens[ start_pos ];
-		
-		/* Check if it needs to be propagated 
-		 * TODO: Add correct checkup 
-		*/
-		if( g->data[0] != -1 )
+		/* locks the signal variable and waits */
+		pthread_mutex_lock( prop_sig_lock );
+		pthread_cond_wait( prop_sig, prop_sig_lock  );
+		pthread_mutex_unlock( prop_sig_lock );
+
+		printf( "[Prop Thread] Recevied signal \n" );
+
+		/* Check if propagation has already been performed */
+		if( cs->prop_gen == -1 )
 		{
-			printf( "[Prop Thread] Propagating generation %d\n", g->num );
+		
+		
+			/* Start at the begining */
+			start_gen = 0;
+			start_pos = 0;
+		
+		}	
+		else
+		{
+			start_gen = cs->prop_gen;
+			start_pos = cs->prop_pos;
 		}
 		
-		/* TODO: Increase with modulus function */
-		start_pos++;
+	
+		/* Fetch the newest generaton that have been inserted */
+		end_gen = cs->max_gen;
+	
+		for( it = start_gen; it <= end_gen; it++ )
+		{
+			/* Fetch the generation */
+			g = cs->gens[ start_pos ];
+		
+			/* Check if it needs to be propagated 
+			 * TODO: Add correct checkup 
+			*/
+			if( g->data[0] != -1 )
+			{
+				printf( "[Prop Thread] Propagating generation %d\n", g->num );
+			}
+		
+			/* TODO: Increase with modulus function */
+			start_pos++;
+		}
+		
+		cs->prop_gen = end_gen + 1;
+		cs->prop_pos = start_pos + 1;
+		
 	}
-
+	
 	pthread_exit( NULL );
 }
