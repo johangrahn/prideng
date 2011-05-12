075e835d0be964879e583eb1db220043b4f9819a
Johan <johan@cgeek.net>
2011-05-12 12:04:55 +0200

    Propagation thread now iterates the generations that needs propagation

--- src/prop.c
+++ src/prop.c
@@ -10,9 +10,12 @@ prop_thread( void* data )
 	pthread_cond_t *prop_sig;
 	pthread_mutex_t *prop_sig_lock;
 	cs_t 			*cs;
+	gen_t 			*g;
 	int 			prop_gen;
 	int 			prop_pos;
-
+	int 			it;
+	int start_gen, start_pos, end_gen, end_pos;
+	
 	prop_sig = ((png_t*)data)->prop_sig;
 	prop_sig_lock = ((png_t*)data)->prop_sig_lock;
 	cs = ((png_t*)data)->cs;
@@ -26,7 +29,36 @@ prop_thread( void* data )
 
 	printf( "[Prop Thread] Recevied signal \n" );
 
+	/* Check if propagation has already been performed */
+	if( cs->prop_gen == -1 )
+	{
+		
+		
+		/* Start at the begining */
+		start_gen = 0;
+		start_pos = 0;
+		
+	}	
 	
+	/* Fetch the newest generaton that have been inserted */
+	end_gen = cs->max_gen;
+	
+	for( it = start_gen; it <= end_gen; it++ )
+	{
+		/* Fetch the generation */
+		g = cs->gens[ start_pos ];
+		
+		/* Check if it needs to be propagated 
+		 * TODO: Add correct checkup 
+		*/
+		if( g->data[0] != -1 )
+		{
+			printf( "[Prop Thread] Propagating generation %d\n", g->num );
+		}
+		
+		/* TODO: Increase with modulus function */
+		start_pos++;
+	}
 
 	pthread_exit( NULL );
 }
